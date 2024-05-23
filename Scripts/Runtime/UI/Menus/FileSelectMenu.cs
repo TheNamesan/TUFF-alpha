24ca1b0b7e05c3edc2617f310aafd6f629cae954
TheNamesan <thenamesan@gmail.com>
2024-05-23 16:12:50 -0600

    SAVE MENU UI DONE!!

--- Scripts/Runtime/UI/Menus/FileSelectMenu.cs
+++ Scripts/Runtime/UI/Menus/FileSelectMenu.cs
@@ -7,26 +7,106 @@ namespace TUFF
     public class FileSelectMenu : MonoBehaviour
     {
         public UIMenu uiMenu;
+        public Transform buttonsParent;
+        public SaveFileHUD saveFileHUDPrefab;
+        public List<SaveFileHUD> elements = new();
         private void Awake()
         {
             if (uiMenu == null) uiMenu = GetComponent<UIMenu>();
-            if (uiMenu != null)
+            //if (uiMenu == null) uiMenu = GetComponent<UIMenu>();
+            //if (uiMenu != null)
+            //{
+            //    if (uiMenu.UIElements == null) uiMenu.SetupUIElements();
+            //    for (int i = 0; i < uiMenu.UIElements.Length; i++)
+            //    {
+            //        if (uiMenu.UIElements[i].Length > 0)
+            //        {
+            //            int index = i;
+            //            var element = uiMenu.UIElements[i][0];
+            //            element.useCustomSelectSFX = true;
+            //            element.customSelectSFX = TUFFSettings.loadSFX;
+            //            element.onSelect.AddListener(() => { SelectFile(index); });
+            //        }
+            //    }
+            //}
+            InitializeSaveFileHUDs();
+        }
+        private void InitializeSaveFileHUDs()
+        {
+            VerifyMenuArrays(0);
+            int index = 0;
+            foreach (Transform child in buttonsParent) // Add existing GameObjects to list
             {
-                if (uiMenu.UIElements == null) uiMenu.SetupUIElements();
-                for (int i = 0; i < uiMenu.UIElements.Length; i++)
+                if (child.TryGetComponent(out SaveFileHUD existing))
                 {
-                    if (uiMenu.UIElements[i].Length > 0)
-                    {
-                        int index = i;
-                        var element = uiMenu.UIElements[i][0];
-                        element.useCustomSelectSFX = true;
-                        element.customSelectSFX = TUFFSettings.loadSFX;
-                        element.text.text = $"File {i + 1}" ; // Change for localization
-                        element.onSelect.AddListener(() => { SelectFile(index); });
-                    }
+                    existing.Initialize(index);
+                    elements.Add(existing);
+                    index++;
+                }
+                
+            }
+            if (saveFileHUDPrefab)
+            {
+                for (; index < 16; index++) // Tmp 16 number
+                {
+                    var create = Instantiate(saveFileHUDPrefab, buttonsParent);
+                    create.Initialize(index);
+                    elements.Add(create);
                 }
             }
-            
+            for (int i = 0; i < elements.Count; i++)
+            {
+                AddToMenu(i, elements[i].fileSelectButton);
+            }
+            if (uiMenu.UIElements == null) uiMenu.SetupUIElements();
+
+            //if (uiMenu != null)
+            //{
+            //    if (uiMenu.UIElements == null) uiMenu.SetupUIElements();
+            //    for (int i = 0; i < uiMenu.UIElements.Length; i++)
+            //    {
+            //        if (uiMenu.UIElements[i].Length > 0)
+            //        {
+            //            int index = i;
+            //            var element = uiMenu.UIElements[i][0];
+            //            element.useCustomSelectSFX = true;
+            //            element.customSelectSFX = TUFFSettings.loadSFX;
+            //            element.onSelect.AddListener(() => { SelectFile(index); });
+            //        }
+            //    }
+            //}
+        }
+        private void VerifyMenuArrays(int rows)
+        {
+            if (uiMenu == null) return;
+            if (uiMenu.UIElementContainers == null)
+            {
+                uiMenu.UIElementContainers = new UIElementContainer[rows];
+            }
+            if (rows >= uiMenu.UIElementContainers.Length) // If row is out of row count
+            {
+                var newArray = new UIElementContainer[rows + 1];
+                System.Array.Copy(uiMenu.UIElementContainers, newArray, uiMenu.UIElementContainers.Length);
+                uiMenu.UIElementContainers = newArray;
+            }
+            if (uiMenu.UIElementContainers[0] == null) // Check Index 0
+                uiMenu.UIElementContainers[0] = new UIElementContainer();
+        }
+        protected void AddToMenu(int index, UIButton element)
+        {
+            if (uiMenu == null) return;
+            if (index >= uiMenu.UIElementContainers.Length)
+            {
+                VerifyMenuArrays(index);
+            }
+            if (uiMenu.UIElementContainers[index] == null)
+                uiMenu.UIElementContainers[index] = new UIElementContainer();
+            uiMenu.UIElementContainers[index].UIElements.Add(element);
+
+            element.useCustomSelectSFX = true;
+            element.customSelectSFX = TUFFSettings.loadSFX;
+            element.onSelect.AddListener(() => SelectFile(index));
+            //element.onSelect.AddListener(() => { SelectFile(index); });
         }
         public void SelectFile(int file)
         {
---
+++ Scripts/Runtime/UI/SaveSystem/SaveFileHUD.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace TUFF
+{
+    public class SaveFileHUD : MonoBehaviour
+    {
+        public UIButton fileSelectButton;
+        public TMP_Text fileText;
+        public List<Image> partyGraphics = new();
+        public TMP_Text timeText;
+        public int index = -1;
+
+        public void Initialize(int index)
+        {
+            this.index = index;
+            UpdateHUD();
+        }
+        public void UpdateHUD()
+        {
+            if (index < 0) return;
+            if (fileText) fileText.text = $"File {index + 1}";
+            var load = PlayerData.LoadData(index);
+            UpdateGraphics(load);
+            if (timeText) timeText.gameObject.SetActive(load != null);
+        }
+
+        private void UpdateGraphics(PlayerData load)
+        {
+            for (int i = 0; i < partyGraphics.Count; i++)
+            {
+                var graphic = partyGraphics[i];
+                if (!graphic) continue;
+                Sprite sprite = null;
+                if (load != null)
+                {
+                    var partyMember = load.GetActivePartyMember(i);
+                    if (partyMember != null) sprite = partyMember.GetGraphic();
+                }
+                graphic.sprite = sprite;
+                graphic.gameObject.SetActive(sprite != null);
+            }
+        }
+    }
+
+}
